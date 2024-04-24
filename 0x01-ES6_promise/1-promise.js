01bfe2f33510762fd89849fd020162dafdac4ea8
lujain <lujain.com>
2024-04-24 22:22:40 +0200

    ES6 Promises

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+  // Check the success parameter
+    if (success) {
+    // If success is true, resolve the promise with a specific object
+      resolve({ status: 200, body: 'Success'});
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
