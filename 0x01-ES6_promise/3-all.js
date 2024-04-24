cf00cd89b21d06cae4a8209a703f38566ac0ea31
lujain <lujain.com>
2024-04-24 22:46:07 +0200

    ES6 Promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+export default function handleProfileSignup() {
+  const photoUploadPromise = uploadPhoto();
+  const userCreationPromise = createUser();
+  return Promise.all([photoUploadPromise, userCreationPromise])
+    .then((answers) => {
+      const photoUploadResult = answers[0].body;
+      const userCreationResult = `${answers[1].firstName} ${answers[1].lastName}`;
+      console.log(`${photoUploadResult} ${userCreationResult}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
