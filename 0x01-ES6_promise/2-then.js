bd81ae1bf5e4d9c962520505116c5a4c92268d3a
lujain <lujain.com>
2024-04-24 22:39:14 +0200

    ES6 Promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  //Append a handler for when the promise resolves
-  promise
-    .then(() => {
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .then(() => {
-      console.log('Got a response from the API');
-    })
-    .catch(() => {
-      return new Error();
-    });
+  console.log('Got a response from the API');
+  return promise
+    .then(() => ({ status: 200, body: 'success'}))
+    .catch(() => new Error());
 }
