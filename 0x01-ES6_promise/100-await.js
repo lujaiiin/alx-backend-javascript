f0b76036dab55a952852c885cfbb1cd430714ea9
lujain <lujain.com>
2024-04-25 00:39:55 +0200

    ES6 Promises

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,15 +4,8 @@ export default async function asyncUploadUser() {
   try {
     const photoResponse = await uploadPhoto();
     const userResponse = await createUser();
-
-    return {
-      photo: photoResponse,
-      user: userResponse,
-    };
+    return { photo: photoResponse, user: userResponse };
   } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    };
+    return { photo: null, user: null };
  }
 }
