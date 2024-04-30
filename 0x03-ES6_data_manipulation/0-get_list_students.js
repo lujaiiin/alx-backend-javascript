429cb5c94265d08981f501c2b419bfb920cad621
lujain <lujain.com>
2024-04-30 20:04:12 +0200

    ES6 data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+function make(id, firstName, location) {
+  return { id, firstName, location };
+}
+
+export default function getListStudents() {
+  return [
+    make(1, 'Guillaume', 'San Francisco'),
+    make(2, 'James', 'Columbia'),
+    make(5, 'Serena', 'San Francisco'),
+  ];
+}
